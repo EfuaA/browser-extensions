2337b0e7d8c04a7a2cde0de7391d7fe4e1bb187d
EfuaA <amezwennes@hotmail.com>
2025-08-03 15:59:30 +0000

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './css/App.css';
 import TitleBar from './components/Title-Bar';
 import Filter from './components/Filter';
+import Extension from './components/Extension';
 
 function App() {
   return (
     <>
       <TitleBar />
       <Filter />
+      <Extension name={'Devlens'} />
     </>
   );
 }
--- src/components/Extension.jsx
+++ src/components/Extension.jsx
@@ -1,1 +1,18 @@
-function Extension({ name }) {}
+import Button from './Button';
+
+function Extension({ name }) {
+  return (
+    <div>
+      {name}
+      <img src="./images/logo-devlens.svg" alt="logo-devlens" />
+      <p>
+        Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
+        De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo
+        vel maleficia?
+      </p>
+      <Button name={'Remove'} />
+    </div>
+  );
+}
+
+export default Extension;
